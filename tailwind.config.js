0c01bf864106542010d21e7a5175757dd27b7b17
Pedro De Almeida Rodrigues <pedrodealmeidarodrigues13@gmail.com>
2024-10-16 16:41:22 +0200

    Gaming Campus

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["assets/src/**/*.{html,js}","*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+    }
+  },
+  plugins: [],
+}
+
